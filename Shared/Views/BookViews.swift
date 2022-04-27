87adb923a46898defe18b01c5120dbb29694a714
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-27 09:38:49 +0900

    Add the book.image struct.

--- Shared/Views/BookViews.swift
+++ Shared/Views/BookViews.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 extension Book {
     struct Image: View {
+        let title: String
         var body: some View {
-            SwiftUI.Image(systemName: "book")
+            let symbol = SwiftUI.Image(title: title) ?? .init(systemName: "book")
+            symbol
                 .resizable()
                 .scaledToFit()
                 .frame(width: 80, height: 80)
@@ -20,8 +22,21 @@ extension Book {
     }
 }
 
+extension Image {
+    init?(title: String) {
+        guard let character = title.first, case let symbolName = "\(character.lowercased()).square", UIImage(systemName: symbolName) != nil else {
+            return nil
+        }
+        self.init(systemName: symbolName)
+    }
+}
+
 struct Book_Previews: PreviewProvider {
     static var previews: some View {
-        Book.Image()
+        VStack {
+            Book.Image(title: Book().title)
+            Book.Image(title: "")
+            Book.Image(title: "🍺")
+        }
     }
 }
