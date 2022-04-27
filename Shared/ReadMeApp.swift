bec623bdd36b7fd8b03891cf3db918d6e907d2de
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-27 09:11:02 +0900

    Initial Commit

---
+++ Shared/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
